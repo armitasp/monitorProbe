32029726451305f602679093319696895d2d1ff5
Scott Armitage <s.p.armitage@lboro.ac.uk>
2015-03-11 14:48:17 +0000

    Update generateCreds.sh

--- etc/eduroam_monitor/generateCreds.sh
+++ etc/eduroam_monitor/generateCreds.sh
@@ -101,5 +101,4 @@ echo } >> /etc/eduroam_monitor/wpa_conf/eduroam-ttls-mschap-anon.conf
   echo anonymous_identity=\"$user@eduroam.ac.uk\" >> /etc/eduroam_monitor/wpa_conf/eduroam-ttls.conf       
   echo phase2=\"auth=EAP-MSCHAPV2\" >> /etc/eduroam_monitor/wpa_conf/eduroam-ttls.conf            
   echo } >> /etc/eduroam_monitor/wpa_conf/eduroam-ttls.conf  
-  
-  
+ 
